8df8ceae9c02f357284ec088247840b88d2ffdd6
bjbrittain06 <bjbrittain06@gmail.com>
2025-02-13 04:36:07 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
-import Head from 'next/head';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Valentine's Day</title>
+        <title>Valentines Day</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="main-content">
-        <Header title="Welcome to my Valentine's Day page!" />
-        <div className="audio-player">
-          <audio controls>
-            <source src="/public/CandC.mp3" type="audio/mp3" />
-            Your browser does not support the audio element.
-          </audio>
-        </div>
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
       </main>
 
       <Footer />
     </div>
-  );
+  )
 }
